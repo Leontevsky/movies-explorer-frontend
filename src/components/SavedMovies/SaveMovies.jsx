2a11c028e609b1a3e63f089bb4c71be2f1bcc42a
leontevsky <alexander.leontevsky@gmail.com>
2022-03-21 12:55:15 +0300

    savedMovies

--- src/components/SavedMovies/SaveMovies.jsx
+++ src/components/SavedMovies/SaveMovies.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-
-import savedCards from '../../constants/saved-cards';
-
 import MoviesPlace from '../MoviesPlace/MoviesPlace';
 import Menu from '../Menu/Menu';
-const SaveMovies = () => {
+const SaveMovies = ({ cardCount, handleDeleteFilm, savedMovies, isAuth }) => {
   return (
     <>
-      <Header homePageBtnCondition={'disable'} />
+      <Header isAuth={isAuth} />
       <Menu activeLinkSavedMovies={'menu__active-link'} />
-      <MoviesPlace
-        savedMoviesMargin={'movies-saved-movies-margin'}
-        cards={savedCards}
-        btnCondishion={'disable'}
-      />
+      <MoviesPlace isSaved={true} cardCount={cardCount} handleDeleteFilm={handleDeleteFilm} savedMovies={savedMovies} />
       <Footer />
     </>
   );
